f44becadd22909b41212853f68ce30b071112d1d
Himanshu Sharma <hs991009@gmail.com>
2025-10-07 20:36:01 +0530

    updated

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from 'body-parser';
-import nodemailer from 'nodemailer';
+import { Resend } from '@resend/node';
 import { config } from 'dotenv';
 config();
+
 const port = process.env.PORT || 8080;
 const app = express();
-const emailUser = process.env.EMAIL_USER;
-const emailPassword = process.env.EMAIL_PASSWORD;
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('home page');
-})
+});
+
 app.post('/mail', async (req, res) => {
     try {
-        const { email,fullname, message } = req.body;
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
-
-        const mailOptions = {
-            from: emailUser,
+        const { email, fullname, message } = req.body;
+        const data = await resend.emails.send({
+            from: 'Himanshusharma<onboarding@resend.dev>', // You can use your verified sender or default
             to: 'werev2022@gmail.com',
             subject: 'Mail from WeRev website',
-            text: `Name:${fullname}\n
-            Email:${email}\n
-            Message:-${message}`
-        };
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent:', info.response);
+            text: `Name: ${fullname}\nEmail: ${email}\nMessage: ${message}`
+        });
+        console.log('Email sent:', data);
         res.json({ success: true, message: 'Email sent successfully' });
-
     } catch (error) {
         console.error('Error sending email:', error);
         res.status(500).json({ error: 'Error sending email' });
@@ -45,4 +36,4 @@ app.post('/mail', async (req, res) => {
 
 app.listen(port, () => {
     console.log('server is running on the', port);
-})
+});
