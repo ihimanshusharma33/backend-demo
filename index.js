8743fd187dbbe80f26782bcfe35a2083d594ce2f
Himanshu Sharma <hs991009@gmail.com>
2025-10-07 20:20:19 +0530

    passoword updated

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.post('/mail', async (req, res) => {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: emailUser,
-                pass: emailPassword
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
             }
         });
 
